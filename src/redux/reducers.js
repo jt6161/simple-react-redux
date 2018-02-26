92bde06714a0206ed0ab45be5b1704166bacaaea
Jennifer Tristan <31359254+jt6161@users.noreply.github.com>
2018-02-26 13:10:21 -0700

    finished exercise

--- src/redux/reducers.js
+++ src/redux/reducers.js
@@ -3,9 +3,17 @@ let initialState = 0
 export const counterReducer = (state = initialState, action) => {
   switch (action.type) {
     case 'INCREMENT':
-      return state + 1
+      if(state < 10) {
+        return state + 1
+      } else {
+        return state
+      }
     case 'DECREMENT':
-      return state - 1
+      if(state > 0) {
+        return state - 1
+      } else {
+        return state
+      }    
     default:
       return state;
   }
